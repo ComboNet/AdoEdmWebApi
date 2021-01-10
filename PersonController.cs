02920d6f7ecde87e9ae7371314a788cde33ce6f8
ComboNet Gaming <73655401+ComboNet@users.noreply.github.com>
2021-01-10 17:59:33 +0700

    Add files via upload

---
+++ PersonController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AdoEdmWebApi.Models;
+
+namespace AdoEdmWebApi.Controllers
+{
+    public class PersonController : ApiController
+    {
+        private testdbEntities db = new testdbEntities();
+
+        // GET: api/Person
+        public IQueryable<Person> GetPeople()
+        {
+            return db.People;
+        }
+
+        // GET: api/Person/5
+        [ResponseType(typeof(Person))]
+        public IHttpActionResult GetPerson(int id)
+        {
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        // PUT: api/Person/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPerson(int id, Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Person
+        [ResponseType(typeof(Person))]
+        public IHttpActionResult PostPerson(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.People.Add(person);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (PersonExists(person.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = person.Id }, person);
+        }
+
+        // DELETE: api/Person/5
+        [ResponseType(typeof(Person))]
+        public IHttpActionResult DeletePerson(int id)
+        {
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            db.People.Remove(person);
+            db.SaveChanges();
+
+            return Ok(person);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PersonExists(int id)
+        {
+            return db.People.Count(e => e.Id == id) > 0;
+        }
+    }
+}
